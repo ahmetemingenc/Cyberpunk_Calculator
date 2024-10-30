241da1575218869b5448e9e69ac862386272e202
Ahmet Emin Genc <ahmetemingenc8@gmail.com>
2024-10-30 17:08:14 +0300

    CyberCalc_X_1.0

---
+++ script.js
@@ -0,0 +1,41 @@
+const screen = document.getElementById('screen');
+
+function appendToScreen(value) {
+    screen.innerText += value;
+}
+
+function clearScreen() {
+    screen.innerText = '';
+}
+
+function deleteChar() {
+    screen.innerText = screen.innerText.slice(0, -1);
+}
+
+function calculateResult() {
+    const input = screen.innerText;
+    
+    // Easter Egg 1: 2077++
+    if (input === '2077++') {
+        screen.innerText = 'Welcome to the CyberCalc X Future!';
+        return;
+    }
+
+    // Easter Egg 2: 8888++
+    if (input === '8888++') {
+        screen.innerText = 'Lucky Number!';
+        return;
+    }
+
+    // Easter Egg 3: 1234++
+    if (input === '1234++') {
+        screen.innerText = 'You found the Secret Number!';
+        return;
+    }
+
+    try {
+        screen.innerText = eval(input);
+    } catch (e) {
+        screen.innerText = 'Error';
+    }
+}
