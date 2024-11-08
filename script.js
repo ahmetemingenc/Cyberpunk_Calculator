c446ca846abc249c837ec0e3f87f6f3402f2cd8b
Ahmet Emin Genc <ahmetemingenc8@gmail.com>
2024-11-08 15:24:39 +0300

    cybercalc_v1.2

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
+// ekran öğesini HTML'den seçip screen değişkenine atıyoruz
 const screen = document.getElementById('screen');
 
+// bu fonksiyon kullanıcıdan gelen veriyi ekrana ekliyor
 function appendToScreen(value) {
-    if (screen.innerText === 'Error') {
+    // ekranda Hata vs. yazıyorsa, kullanıcı başka bir işlem yapmak istediğinde ekranı temizlemek için if kullanıyoruz
+
+    if (screen.innerText === 'Hata') {
         clearScreen();
     }
+
+    if (screen.innerText === 'Geleceğe Hoş Geldin!') {
+        clearScreen();
+    }
+
+    if (screen.innerText === 'Hayat, Evren ve Her Şeyin Anlamı!') {
+        clearScreen();
+    }
+
+    // argumentten gelen değeri ekrandaki mevcut değerin sonuna ekliyoruz
     screen.innerText += value;
 }
 
+// ekrandaki tüm metni silen fonksiyon, html'deki C butonuna bağlı
 function clearScreen() {
     screen.innerText = '';
 }
 
+// ekrandaki son karakteri silen fonksiyon, html'deki SİL butonuna bağlı
 function deleteChar() {
+    // ekrandaki metnin son karakterini kesip güncelleyerek siliyoruz.
     screen.innerText = screen.innerText.slice(0, -1);
 }
 
+// ekrandaki matematiksel ifadeyi değerlendirip sonucu hesaplayan fonksiyon, html'deki = butonuna bağlı
 function calculateResult() {
+    // girilen değeri alıyoruz
     const input = screen.innerText;
     
-    // Easter Egg 1: 2077++
+    // basit hoşluklar eklemek için if ifadeleri ekledim
+    // normalde herhangi bir işlem operatörü iki kez üst üste yazılırsa hata mesajı alırız 
+    // ancak spesifik ifadeler için if kullanarak catch'e yakalanmasını engelliyoruz
+
+    // kullanıcı 2077++ değerini girerse ekranda "'Geleceğe Hoş Geldin!" yazacak
     if (input === '2077++') {
-        screen.innerText = 'Welcome to the CyberCalc X Future!';
+        screen.innerText = 'Geleceğe Hoş Geldin!';
+        return;
+    }
+    // kullanıcı 42++ değerini girerse ekranda "Hayat, Evren ve Her Şeyin Anlamı!" yazacak
+    if(input ==='42++'){
+        screen.innerText = 'Hayat, Evren ve Her Şeyin Anlamı!';
         return;
     }
 
-    // Easter Egg 2: 8888++
-    if (input === '8888++') {
-        screen.innerText = 'Lucky Number!';
-        return;
-    }
-
-    // Easter Egg 3: 1234++
-    if (input === '1234++') {
-        screen.innerText = 'You found the Secret Number!';
-        return;
-    }
-
+    // girilen matematiksel ifadeyi hesaplamaya çalışıyoruz. Burada try catch bloğu kullanarak hatalı girişlerde "Hata" mesajı gösteriyoruz
     try {
+        // eval fonksiyonuyla işlemi hesaplayıp sonucu ekrana yazıyoruz
         screen.innerText = eval(input);
     } catch (e) {
-        screen.innerText = 'Error';
+        // bir hata olursa "Hata" mesajını gösteriyoruz
+        screen.innerText = 'Hata';
     }
 }
